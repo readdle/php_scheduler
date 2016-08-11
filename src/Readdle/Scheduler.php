72fc72bf371ec721d238413dde1c9ae9f6277297
Vladimir Melnik <melnik.wladimir@gmail.com>
2016-08-11 18:33:24 +0300

    - time-table fixes

--- src/Readdle/Scheduler.php
+++ src/Readdle/Scheduler.php
@@ -3,11 +3,15 @@ namespace Readdle\Scheduler;
 
 class Scheduler
 {
-    const ONE_DAY_INTERVAL = 86400;
+    const ONE_MINUTE_INTERVAL = 60;
+    const FIVE_MINUTE_INTERVAL = 300;
+
     const ONE_HOUR_INTERVAL = 3600;
     const TWO_HOUR_INTERVAL = 7200;
     const FOUR_HOUR_INTERVAL = 14400;
-    const FIVE_MINUTE_INTERVAL = 300;
+
+    const ONE_DAY_INTERVAL = 86400;
+
 
     protected $dataStorage;
     protected $scripts = [];
@@ -21,8 +25,8 @@ class Scheduler
 
     public function register($interval, $script)
     {
-        if ($interval < 1) {
-            throw new \Exception("Interval cannot be less that 1 sec.");
+        if ($interval < 2) {
+            throw new \Exception("Interval cannot be less that 2 sec.");
         }
 
         if (!method_exists($script, "getName")) {
@@ -31,42 +35,29 @@ class Scheduler
             $scriptName = $script->getName();
         }
 
-        $this->scripts[$scriptName] = [
+        $this->scripts["{$scriptName}:{$interval}"] = [
             'callable' => $script,
             'interval' => $interval
         ];
     }
 
-    protected function calcNextScriptRunTime($scriptName)
-    {
-        $lasScriptRunTime = (int)$this->dataStorage->get($scriptName);
-        $lasScriptRunTime = $this->getCronLikeTime($lasScriptRunTime, $this->scripts[$scriptName]['interval']);
-
-        $nextScriptRunTime = $this->scripts[$scriptName]['interval'] + $lasScriptRunTime;
-
-        if (null === $this->nextRun or $this->nextRun > $nextScriptRunTime) {
-            $this->nextRun = $nextScriptRunTime;
-        }
-
-        return $nextScriptRunTime;
-    }
-
     /**
      * Run scripts like cron: 5 minute -> every 3:00, 3:05, 3:10
-     * @param int $time
      * @param int $interval
      * @return int
      */
-    protected function getCronLikeTime($time, $interval)
+    protected function getCronLikeTimeSinceNow($interval)
     {
         $adjustSeconds = 0;
 
+        $time = time();
         $mod = $time % $interval;
         if ($mod > 0) {
             $adjustSeconds = $interval - $mod;
         }
 
-        return $time + $adjustSeconds;
+        $normalized = ($time + $adjustSeconds);
+        return (time() === $normalized) ? ($normalized + $interval) : $normalized;
     }
 
     public function loop()
@@ -75,22 +66,34 @@ class Scheduler
 
         while (true) {
             foreach ($this->scripts as $scriptName => $scriptDetails) {
-                $nextRun = $this->calcNextScriptRunTime($scriptName);
+                $nextRun = (int)$this->dataStorage->get($scriptName);
 
                 if ($nextRun > time()) {
                     continue;
                 }
 
-                $scriptDetails['callable']();
-                $this->dataStorage->save($scriptName, time());
+                # running script
+                call_user_func($scriptDetails['callable']);
+
+                # setup time for next running
+                $nextRun = $this->getCronLikeTimeSinceNow($scriptDetails['interval']);
+                $this->updateNextRun($nextRun);
+                $this->dataStorage->save($scriptName, $nextRun);
             }
 
             if ($this->nextRun > time()) {
-                time_sleep_until($this->nextRun + 1);
+                time_sleep_until($this->nextRun);
             } else {
                 time_sleep_until(time() + 1);
             }
             $this->nextRun = null;
+        }
+    }
+
+    protected function updateNextRun($nextRun)
+    {
+        if ((null === $this->nextRun) || ($nextRun < $this->nextRun)) {
+            $this->nextRun = $nextRun;
         }
     }
 }
--- test.php
+++ test.php
@@ -1,10 +1,3 @@
-**General scheduler project**
-
-- Run tests inside docker container `docker-compose up`
-
-**Example usage**
-
-```php
 <?php
 
 include "./vendor/autoload.php";
@@ -45,20 +38,15 @@ $storage = new Storage();
 
 // Create scheduler object
 $scheduler = new \Readdle\Scheduler\Scheduler(
-    new \Readdle\Scheduler\PersistentStorage(
-        'test',
-        [$storage, 'set'],
-        [$storage, 'get']
-    )
+    new \Readdle\Scheduler\PersistentStorage('test', [$storage, 'set'], [$storage, 'get'])
 );
 
 // Register your scripts
+$scheduler->register(5, new SomeScript('5 second'));
 $scheduler->register(10, new SomeScript('10 second'));
-$scheduler->register(20, new SomeScript('20 second'));
-$scheduler->register(7, new SomeScript('7 second'));
 $scheduler->register(3, new SomeScript('3 second'));
-$scheduler->register(27, new SomeScript('27 second'));
+$scheduler->register(7, new SomeScript('7 second'));
+$scheduler->register(15, new SomeScript('15 second'));
 
 // Start scheduler loop
 $scheduler->loop();
-```
